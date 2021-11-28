a17c714e66d95a7f5b0c5f3b9edcedcec2305915
ChandanGowda574 <69746473+ChandanGowda574@users.noreply.github.com>
2021-11-28 21:53:10 +0530

    Delete SpringAssignment/SpringRestAssignments directory

--- SpringAssignment/SpringRestAssignments/Rest5_EmployeeManagement/src/main/java/com/priyanka/yadav/repositories/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.priyanka.yadav.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.priyanka.yadav.models.Employee;
-
-public interface EmployeeRepository extends CrudRepository<Employee, String> {
-	
-}
--- SpringAssignment/SpringRestAssignments/Rest9_Customer/src/main/java/com/priyanka/yadav/repository/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.priyanka.yadav.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.priyanka.yadav.model.Customer;
-
-public interface CustomerRepository extends CrudRepository<Customer, String> {
-	
-}
